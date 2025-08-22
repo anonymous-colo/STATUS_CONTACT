b0f90d9496e30816fda85e558fb82c28d086ad14
anonymous-colo <colonelgwozozo4777@gmail.com>
2025-08-22 13:20:56 -0500

    Ajout/Mise à jour de client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,38 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { LanguageProvider } from "@/components/LanguageProvider";
+import Home from "@/pages/Home";
+import Admin from "@/pages/Admin";
+import NotFound from "@/pages/not-found";
+import "./lib/i18n";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/admin" component={Admin} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <LanguageProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Router />
+          </TooltipProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
