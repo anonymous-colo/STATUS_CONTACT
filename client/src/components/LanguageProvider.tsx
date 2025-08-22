438f859e50afb94fbe6643c543027ab9ad6df244
anonymous-colo <colonelgwozozo4777@gmail.com>
2025-08-22 13:20:59 -0500

    Ajout/Mise à jour de client/src/components/LanguageProvider.tsx

---
+++ client/src/components/LanguageProvider.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext } from "react";
+import { useTranslation } from "react-i18next";
+
+type LanguageContextType = {
+  currentLanguage: string;
+  changeLanguage: (lang: string) => void;
+  t: (key: string) => string;
+};
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+
+export function LanguageProvider({ children }: { children: React.ReactNode }) {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    localStorage.setItem("language", lang);
+  };
+
+  return (
+    <LanguageContext.Provider
+      value={{
+        currentLanguage: i18n.language,
+        changeLanguage,
+        t,
+      }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  const context = useContext(LanguageContext);
+  if (context === undefined) {
+    throw new Error("useLanguage must be used within a LanguageProvider");
+  }
+  return context;
+}
